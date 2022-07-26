eaefc20163acadc731cc6da25b9e3e06f5e9f6ba
Lawrence Oluwasegun <lawrencesegun025@gmail.com>
2022-07-26 23:52:11 +0100

    Linked List

--- Linear Data structure/LinkedList/README.md
+++ Linear Data structure/LinkedList/README.md
@@ -3,3 +3,11 @@
 Linked list is a collection of data links called nodes, where a node contains a data value and a pointer(memory address) of the next link(node).
 
 Linked List are dynamic in nature
+
+Output of the code we would be written in the main.go file
+
+
+## Screenshots
+
+![App Screenshot](https://i.postimg.cc/3JMvqv1m/Screenshot-from-2022-07-26-23-02-07.png)
+
--- Linear Data structure/LinkedList/main.go
+++ Linear Data structure/LinkedList/main.go
@@ -12,38 +12,67 @@ type linkedList struct {
 	length int
 }
 
-func (l *linkedList) AddNode(value int) {
+func (l *linkedList) AddNodeToBeginningOfThelist(value int) {
 	newNode := &node{
 		value: value,
 		next:  nil,
 	}
 	if l.head == nil {
-		l.head = newNode
-		l.length++
+		l.head = newNode //If there's no node yet, it's will create one.
+		l.length++       // Increment the length of the list.
 	} else {
-		l.head = newNode
 		newNode.next = l.head
+		l.head = newNode
 		l.length++
 	}
+}
+
+func (l *linkedList) deleteNodeWithValue(value int) {
+	if l.length == 0 {
+		return
+	}
+	if l.head.value == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	previousToDelete := l.head
+	for previousToDelete.next.value != value {
+		if previousToDelete.next.next == nil {
+			return
+		}
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
 }
 
 func (l linkedList) Printlist() {
 	if l.head == nil {
-		return
+		return //If there are no nodes, it should not print anything.
 	} else {
-		currentNode := l.head
-		for currentNode != nil {
-			fmt.Println(currentNode.value)
+		currentNode := l.head    //If there's are nodes, it will set the current node to the first node in the list.
+		for currentNode != nil { // Iterate through the list until the current node is pointing to nil.
+			listoutput := fmt.Sprintf("%v -> ", currentNode.value)
 			currentNode = currentNode.next
+			fmt.Print(listoutput)
 		}
+		fmt.Print(nil, "\n")
 	}
-
 }
 
 func main() {
 	list := linkedList{}
-	list.AddNode(5)
-	list.AddNode(6)
-	list.AddNode(8)
+	list.AddNodeToBeginningOfThelist(5)
+	fmt.Println(list.head) //The output for this will be nil because it has no node it's pointing to.
+	list.AddNodeToBeginningOfThelist(6)
+	list.AddNodeToBeginningOfThelist(7)
+	list.AddNodeToBeginningOfThelist(8)
+	fmt.Println(list.head) // The output for this will be the address/pointer(memory location) of node with value 7
+	list.AddNodeToBeginningOfThelist(9)
+	list.AddNodeToBeginningOfThelist(10)
+
 	list.Printlist()
+	fmt.Println(list.head) // the output for this will eb address/ pointer(memory location) of 9
 }
